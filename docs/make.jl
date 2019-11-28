1eacfc06cd477cff9a6e7529eaad9d71e5f951ca
Goysa2 <samuel.goyette@usherbrooke.ca>
2019-11-28 17:46:22 -0500

    new tests for docs

--- docs/make.jl
+++ docs/make.jl
@@ -4,8 +4,4 @@ makedocs(modules = [State],
 		 format = Documenter.HTML(assets = ["assets/style.css"], prettyurls = get(ENV, "CI", nothing) == "true"),
 		 sitename = "State.jl")
 
-deploydocs(deps = nothing, make = nothing,
-		   repo = "github.com/Goysa2/State.jl.git",
-		   target = "build",
-		   devbranch = "master"
-           )
+deploydocs(repo = "github.com/Goysa2/State.jl.git")
