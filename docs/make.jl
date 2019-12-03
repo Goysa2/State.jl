e054c49fb41bd830f6c02d96f11a9085b0845eff
Goysa2 <samuel.goyette@usherbrooke.ca>
2019-12-03 13:53:17 -0500

    try to fix make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -5,12 +5,9 @@ makedocs(
     sitename = "State.jl",
     format = Documenter.HTML(assets = ["assets/style.css"], prettyurls = get(ENV, "CI", nothing) == "true"),
     modules = [State],
-    doctest = true,
-    linkcheck = true,
-    strict = true
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-deploydocs(repo = "github.com/Goysa2/State.jl", devbranch = "master")#
+deploydocs(repo = "github.com/Goysa2/State.jl")#
