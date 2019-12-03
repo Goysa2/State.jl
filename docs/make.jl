0d8c0c598f8355a63347a188a1e9a3de3ad66b2e
Goysa2 <samuel.goyette@usherbrooke.ca>
2019-12-03 13:26:33 -0500

    removal of test-doc branch

--- docs/make.jl
+++ docs/make.jl
@@ -13,4 +13,4 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-deploydocs(repo = "github.com/Goysa2/State.jl", devbranch = "test-doc")#
+deploydocs(repo = "github.com/Goysa2/State.jl", devbranch = "master")#
